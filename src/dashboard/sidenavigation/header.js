3170c5cf0422b94eae5ee09a47349ad71c375028
enochndika <enochbeloved@gmail.com>
2021-07-30 04:44:58 +0100

    update sidenav header logo

--- src/dashboard/sidenavigation/header.js
+++ src/dashboard/sidenavigation/header.js
@@ -1,12 +1,7 @@
 export default function SidenavHeader() {
   return (
     <div className="bg-white flex items-center justify-center mb-6 pb-6 sticky top-0 z-10">
-      <img
-        src="https://res.cloudinary.com/beloved/image/upload/v1624365798/Assets/lucha_xmxkfy.png"
-        width={120}
-        height={120}
-        alt="Lucha y vence"
-      />
+      <img src="/images/2.png" width={120} height={120} alt="Lucha y vence" />
     </div>
   );
 }
